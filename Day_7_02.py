15e0b36197c2a9df9435ce95f55b630a0d167318
Skips-Scramble <skipsscramble24601@gmail.com>
2021-12-07 22:01:00 -0500

    Update Day_7_02.py

--- Day_7_02.py
+++ Day_7_02.py
@@ -4,18 +4,18 @@ input = input_file.read().splitlines()
 
 init_depths = [int(x) for x in input[0].split(",")]
 
-max_y = max(test)
-min_y = min(test)
+max_y = max(init_depths)
+min_y = min(init_depths)
 
 def fuel_count(target):
     fuel = 0
     for item in init_depths:
-        fuel = fuel + abs(item - target)
+        fuel = fuel + (((abs(item - target) + 1) * abs(item - target)) / 2)
     return fuel
 
 # Do something where you will go higher or lower as your guesses change
 
-min_fuel = (max_y - min_y) * len(init_depths)
+min_fuel = fuel_count(max_y)
 
 for i in range(min_y, max_y):
     fuel_count_i = fuel_count(i)
