a61dc22c061dbc37bb66317ad50acdee04ed9b49
Sam Miller <Sam.Miller@milliman.com>
2021-12-14 09:09:02 -0500

    Update Day_9_02.py

--- Day_9_02.py
+++ Day_9_02.py
@@ -7,38 +7,36 @@ input = input_file.read().splitlines()
 
 # Note: basins can have flat part to them (every point is part of a basin that's not a 9)
 
-test = []
+ref_input = []
 
-for item in input[:2]:
+for item in input:
+    new_item = str()
     for i in range(1, len(item)):
-        if i == 9:
-            item[i].replace(str(i), "1")
+        if item[i] != "9":
+            new_item = new_item + "0"
         else:
-            item.replace(str(i), "0")
-    test.append(item)
+            new_item = new_item + "1"
+    ref_input.append(new_item)
+
+conf_indices = set()
 
 
-def check_col(row, col):
-    adj_vert = []
+def check_surroundings(row, col):
     for i in {-1, 1}:
-        if row + i >= 0 and row + i <= len(input) - 1:
-            adj_vert.append(input[row + i][col])
+        if row + i >= 0 and row + i <= len(ref_input) - 1:
+            if ref_input[row + i][col] != 1:
+                conf_indices.add((row + i, col))
 
-    return adj_vert
-
-
-def check_row(row, col):
-    adj_horiz = []
     for i in {-1, 1}:
-        if col + i >= 0 and col + i <= len(input[0]) - 1:
-            adj_horiz.append(input[row][col + i])
+        if col + i >= 0 and col + i <= len(ref_input[0]) - 1:
+            if ref_input[row][col + i] != 1:
+                conf_indices.add((row, col + i))
 
-    return adj_horiz
+    return conf_indices
 
 
-check_col(4, 9)
+check_surroundings(1, 2)
 
-check_row(4, 9)
 
 heights = 0
 for i, j in itertools.product(range(len(input)), range(len(input[0]))):
