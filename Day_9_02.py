f54148e237afabeaded1c4b662d4e5e5447303a3
Sam Miller <Sam.Miller@milliman.com>
2021-12-16 09:17:08 -0500

    Update Day_9_02.py

--- Day_9_02.py
+++ Day_9_02.py
@@ -1,5 +1,3 @@
-import itertools
-
 input_file = open(
     "H:\Personal\PRM\SkipsScramble_Repo\Advent_of_Code_2021\input_09_sample.txt"
 )
@@ -11,7 +9,7 @@ ref_input = []
 
 for item in input:
     new_item = str()
-    for i in range(1, len(item)):
+    for i in range(0, len(item)):
         if item[i] != "9":
             new_item = new_item + "0"
         else:
@@ -24,25 +22,27 @@ conf_indices = set()
 def check_surroundings(row, col):
     for i in {-1, 1}:
         if row + i >= 0 and row + i <= len(ref_input) - 1:
-            if ref_input[row + i][col] != 1:
+            if ref_input[row + i][col] != "1":
                 conf_indices.add((row + i, col))
 
     for i in {-1, 1}:
         if col + i >= 0 and col + i <= len(ref_input[0]) - 1:
-            if ref_input[row][col + i] != 1:
+            if ref_input[row][col + i] != "1":
                 conf_indices.add((row, col + i))
 
     return conf_indices
 
 
-check_surroundings(1, 2)
+check_surroundings(2, 3)
 
+add_points = True
 
-heights = 0
-for i, j in itertools.product(range(len(input)), range(len(input[0]))):
-    if input[i][j] < min(check_col(i, j) + check_row(i, j)):
-        print(i, j)
-        heights = heights + int(input[i][j]) + 1
-
-print(heights)
+while add_points == True:
+    conf_indices = {(0, 9)}
+    for point in conf_indices:
+        old_len = len(conf_indices)
+        conf_indices.union(check_surroundings(point[0], point[1]))
+        new_len = len(conf_indices)
+        if new_len > old_len:
+            add_points = False
 
